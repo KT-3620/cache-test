0779b819d806de91c7c60ff87f5641911e200856
KT-3620 <kt.app.support@icloud.com>
2023-11-10 22:33:54 +0900

    キャッシュ設定を追加

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,10 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+  res.setHeader("Cache-Control", "s-maxage=10");
+  const now = new Date();
+  return res.status(200).json({
+    msg: "キャッシュのテストです。",
+    now: `${now.toISOString()}`,
+  });
 }
